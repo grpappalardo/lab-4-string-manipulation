9c22868ad9de1432c7928ada033e5a738208e58f
Gloria Pappalardo <60324395+grpappalardo@users.noreply.github.com>
2020-09-25 17:50:20 -0400

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,5 +1,24 @@
-# Write code to extract the latitude and longitude from the URL: https://www.google.com/maps/@42.2509428,-71.8249939,17z
-# Output should be text that reads:
+'''
+In this exercise I used RegEx to find parts of the URL that are numerical, @, ., or -. Then I indexed to take off the
+@, split at the comma, and printed the latitude/longitude with concatenation.
 
-# Latitude: 42.2509428
-# Longitude: -71.8249939
+'''
+
+import re
+url = 'https://www.google.com/maps/@42.2509428,-71.8249939,17z'
+# x = re.findall('@[0-9\.-]+,[0-9\.-]+', url)
+x = re.findall('@[0-9\.-]+,[0-9\.-]+', url)
+
+x_str = ''.join(x)
+
+xstr = x[0]
+print(xstr)
+
+xstr = xstr[1:len(xstr)]
+print(xstr)
+xsplit = xstr.split(',')
+print(xsplit)
+
+print('Latitude: ' + xsplit[0] + '\n' + 'Longitude: ' + xsplit[1])
+
+latlong_output('https://www.google.com/maps/@42.2509428,-71.8249939,17z')
