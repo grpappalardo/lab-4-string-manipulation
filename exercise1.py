d9558403596a3c91d8338050928986ed95b60c0f
Gloria Pappalardo <60324395+grpappalardo@users.noreply.github.com>
2020-09-25 17:49:04 -0400

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,17 @@
-#Write a function that capitalizes the first and fourth letters of a name, e.g.
+'''
+Name: Gloria Pappalardo
+Date created: 9/18/20
+Version of Python: 3.8
+The input is a word, then indexing is ued the separate the word into two parts, capitalizing the start of each one.
+Then concatenation is used to put the two capitalized parts back together.
 
-#old_macdonald('macdonald') --> MacDonald
+'''
 
-#Add your code to exercise1.py
-#Note: there is a method called "capitalize." The syntax for it is 'macdonald'.capitalize() and it returns 'Macdonald'
+def capitalize_first_fourth(word):
+# variables created to separate the argument into two parts
+    x = word[0:3]
+    y = word[3:len(word)]
+# prints the sections concatenated and capitalized with the .capitalize() method
+    print (x.capitalize() + y.capitalize())
+
+capitalize_first_fourth('copernicus')
